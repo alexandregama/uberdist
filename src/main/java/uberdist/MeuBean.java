1f0d8ceb2dfbaa9f86a057a37a9f3afad52b3ccb
alexandregama <alexandre.gama.lima@gmail.com>
2015-08-24 21:32:47 -0300

    Adiciona dependencias de Servlet e JSF 2.2 no pom.xml e cria ManagedBea de teste

---
+++ src/main/java/uberdist/MeuBean.java
@@ -0,0 +1,14 @@
+package uberdist;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class MeuBean {
+
+	private String nome = "Alexandre Gama";
+			
+	public String getNome() {
+		return nome;
+	}
+	
+}
